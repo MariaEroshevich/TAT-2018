152acfb475c32a7e32f19f13b9fcd24995b3f4b4
MariaEroshevich <m.eroshevich97@gmail.com>
2018-03-03 00:52:50 +0300

    The task_DEV-1 project files were added.

---
+++ task_DEV-1/task_DEV-1/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task_DEV_1
+{
+  public class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      SequenceChecker sequenceChecker = new SequenceChecker(args[0]);
+      Console.WriteLine("The maximum duplicate successive symbols: " + sequenceChecker.SearchMaxNumberOfDuplicates());
+    }
+  }
+}
