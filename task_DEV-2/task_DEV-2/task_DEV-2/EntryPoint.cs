076d6d3e51158f100a9b6b44f3bcc62f742a155c
MariaEroshevich <m.eroshevich97@gmail.com>
2018-03-04 19:23:44 +0300

    The task_DEV-2 project files were added.

---
+++ task_DEV-2/task_DEV-2/task_DEV-2/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_DEV_2
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter your string: ");
+      string line;
+      do
+      {
+        line = Console.ReadLine();
+        if (line == string.Empty)
+        {
+          Console.WriteLine("You entered the empty string. Please, enter again: ");
+        }
+      }
+      while (line == string.Empty);
+      EvenIndexesFinder finder = new EvenIndexesFinder();
+      Console.WriteLine("The string with even symbols: " + finder.FindEvenSymbols(line));
+    }
+  }
+}
